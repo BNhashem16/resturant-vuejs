eab580f58e2c7beb845612e9516702056e427636
Ahmed Hashem <hashem.codes@gmail.com>
2023-06-22 08:01:17 +0300

    first commit

---
+++ src/components/Signup/SignupForm.vue
@@ -0,0 +1,52 @@
+<template>
+    <form @click.prevent>
+        <div class="row g-3 align-items-center">
+            <h1>Signup</h1>
+            <div class="col-auto d-block mx-auto">
+                <input
+                    type="text"
+                    class="form-control"
+                    name=""
+                    placeholder="Enter your name"
+                />
+            </div>
+        </div>
+
+        <div class="row g-3 align-items-center">
+            <div class="col-auto d-block mx-auto">
+                <input
+                    type="text"
+                    class="form-control"
+                    name=""
+                    placeholder="Enter your email"
+                />
+            </div>
+        </div>
+
+        <div class="row g-3 align-items-center">
+            <div class="col-auto d-block mx-auto">
+                <input
+                    type="text"
+                    class="form-control"
+                    name=""
+                    placeholder="Enter your password"
+                />
+            </div>
+        </div>
+
+        <div class="row g-3 align-items-center">
+            <div class="col-auto d-block mx-auto">
+                <button type="submit" class="btn btn-primary">Signup</button>
+            </div>
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+    name: 'SignupForm',
+}
+</script>
+
+<style>
+</style>
---
+++ src/views/Signup.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+   
+        <SignupForm></SignupForm>
+    </div>
+</template>
+
+<script>
+import SignupForm from '@/components/Signup/SignupForm.vue'
+
+export default {
+    name: 'Signup',
+    components: {
+        SignupForm,
+    },
+}
+</script>
+
+<style>
+</style>
