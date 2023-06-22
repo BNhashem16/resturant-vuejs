6c77f4259901cad092282ed1adaca2e246c3aeff
Ahmed Hashem <hashem.codes@gmail.com>
2023-06-22 08:15:19 +0300

    Fix Design

--- src/components/Signup/SignupForm.vue
+++ src/components/Signup/SignupForm.vue
@@ -2,41 +2,41 @@
     <form @click.prevent>
         <div class="row g-3 align-items-center">
             <h1>Signup</h1>
-            <div class="col-auto d-block mx-auto">
+            <div class="col-4 d-block mx-auto my-3">
                 <input
                     type="text"
                     class="form-control"
-                    name=""
+                    v-model="name"
                     placeholder="Enter your name"
                 />
             </div>
         </div>
 
         <div class="row g-3 align-items-center">
-            <div class="col-auto d-block mx-auto">
+            <div class="col-4 d-block mx-auto my-3">
                 <input
                     type="text"
                     class="form-control"
-                    name=""
+                    v-model="email"
                     placeholder="Enter your email"
                 />
             </div>
         </div>
 
         <div class="row g-3 align-items-center">
-            <div class="col-auto d-block mx-auto">
+            <div class="col-4 d-block mx-auto my-3">
                 <input
                     type="text"
                     class="form-control"
-                    name=""
+                    v-model="password"
                     placeholder="Enter your password"
                 />
             </div>
         </div>
 
         <div class="row g-3 align-items-center">
-            <div class="col-auto d-block mx-auto">
-                <button type="submit" class="btn btn-primary">Signup</button>
+            <div class="col-4 d-block mx-auto my-3">
+                <button type="submit" class="btn btn-primary w-100">Signup</button>
             </div>
         </div>
     </form>
@@ -45,6 +45,13 @@
 <script>
 export default {
     name: 'SignupForm',
+    data() {
+        return {
+            name: '',
+            password: '',
+            email: '',
+        }
+    }
 }
 </script>
 
