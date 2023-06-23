3701b385540fb9e51bd22fb453839807201a0935
Ahmed Hashem <hashem.codes@gmail.com>
2023-06-23 15:40:19 +0300

    vuelidate

--- src/components/Signup/LoginForm.vue
+++ src/components/Signup/LoginForm.vue
@@ -49,6 +49,7 @@ export default {
             email: '',
         }
     },
+
     methods: {
         ...mapActions(['redirectTo']),
     },
--- src/components/Signup/SignupForm.vue
+++ src/components/Signup/SignupForm.vue
@@ -9,17 +9,24 @@
                     v-model="name"
                     placeholder="Enter your name"
                 />
+                <div class="error" v-if="v$.name.$error">
+                    {{ v$.name.$errors[0].$message }}
+                </div>
             </div>
         </div>
 
         <div class="row g-3 align-items-center">
             <div class="col-4 d-block mx-auto my-3">
                 <input
-                    type="text"
+                    type="email"
                     class="form-control"
                     v-model="email"
                     placeholder="Enter your email"
                 />
+
+                <div class="error" v-if="v$.email.$error">
+                    {{ v$.email.$errors[0].$message }}
+                </div>
             </div>
         </div>
 
@@ -36,12 +43,16 @@
 
         <div class="row align-items-center">
             <div class="col-6 mx-auto">
-                <button type="submit" class="col-4 btn btn-primary m-1">
+                <button
+                    type="submit"
+                    @click="signup()"
+                    class="col-4 btn btn-primary m-1"
+                >
                     Signup
                 </button>
                 <button
                     type="button"
-                    @click="redirectTo({ name: 'login'})"
+                    @click="redirectTo({ name: 'login' })"
                     class="col-4 btn btn-primary m-1"
                 >
                     Login
@@ -52,9 +63,13 @@
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions } from 'vuex'
+import { useVuelidate } from '@vuelidate/core'
+import { required, email, minLength } from '@vuelidate/validators'
+
 export default {
     name: 'SignupForm',
+    setup: () => ({ v$: useVuelidate() }),
     data() {
         return {
             name: '',
@@ -62,8 +77,17 @@ export default {
             email: '',
         }
     },
+    validations() {
+        return {
+            name: { required, minLength: minLength(6) },
+            email: { required, email },
+        }
+    },
     methods: {
         ...mapActions(['redirectTo']),
+        signup() {
+            this.v$.$validate()
+        },
     },
 }
 </script>
