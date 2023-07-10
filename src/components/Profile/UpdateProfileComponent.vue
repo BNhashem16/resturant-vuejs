cf0a0f24a00602cda6c39093f06d459a4d5acee7
Ahmed Hashem <hashem.codes@gmail.com>
2023-07-10 04:36:25 +0300

    update profile

--- src/components/Profile/UpdateProfileComponent.vue
+++ src/components/Profile/UpdateProfileComponent.vue
@@ -10,6 +10,9 @@
                     v-model="name"
                     placeholder="Enter your name"
                 />
+                <div class="error" v-if="v$.name.$error">
+                    {{ v$.name.$errors[0].$message }}
+                </div>
             </div>
         </div>
 
@@ -21,6 +24,10 @@
                     v-model="email"
                     placeholder="Enter your email"
                 />
+
+                <div class="error" v-if="v$.email.$error">
+                    {{ v$.email.$errors[0].$message }}
+                </div>
             </div>
         </div>
 
@@ -32,6 +39,10 @@
                     v-model="password"
                     placeholder="Enter your password"
                 />
+
+                <div class="error" v-if="v$.password.$error">
+                    {{ v$.password.$errors[0].$message }}
+                </div>
             </div>
         </div>
 
@@ -50,20 +61,54 @@
 </template>
 
 <script>
+import useValidate from '@vuelidate/core'
+import { required, email } from '@vuelidate/validators'
+import axiosInstance from '@/axiosInstance'
+import { mapActions } from 'vuex'
+
 export default {
     methods: {
-        updateProfile() {},
+        ...mapActions(['redirectTo']),
+        async updateProfileNow() {
+            this.v$.$validate()
+            if (!this.v$.$error) {
+                console.log('valid')
+                let result = await axiosInstance.put(`users/${this.id}`, {
+                    name: this.name,
+                    email: this.email,
+                    password: this.password,
+                })
+                if (result.status == 200) {
+                    localStorage.setItem('user', JSON.stringify(result.data))
+                    this.redirectTo({name: 'profile'})
+                } else {
+
+                }
+            } else {
+                console.log('not valid')
+            }
+        },
     },
     data() {
         return {
+            v$: useValidate(),
+            id: '',
             name: '',
             email: '',
             password: '',
         }
     },
+    validations() {
+        return {
+            name: { required },
+            email: { required, email },
+            password: { required },
+        }
+    },
     mounted() {
         let user = localStorage.getItem('user')
         if (user) {
+            this.id = JSON.parse(user).id
             this.name = JSON.parse(user).name
             this.email = JSON.parse(user).email
             this.password = JSON.parse(user).password
