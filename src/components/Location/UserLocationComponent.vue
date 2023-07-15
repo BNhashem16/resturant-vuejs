90f9fe69925332a2e24253995ac4079a6484b2ee
Ahmed Hashem <hashem.codes@gmail.com>
2023-07-16 02:24:24 +0300

    View Each User Locations

---
+++ src/components/Location/UserLocationComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <table class="table caption-top" v-if="allLocations.length > 0">
+        <caption>
+            List of Locations ({{
+                allLocations.length
+            }})
+        </caption>
+
+        <thead class="table-dark">
+            <tr>
+                <th>Name</th>
+                <th>Phone</th>
+                <th>Number</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(location, key) in allLocations" :key="key">
+                <td>{{ location.name }}</td>
+                <td>{{ location.phone }}</td>
+                <td>{{ location.address }}</td>
+                <td>{{ location.address }}</td>
+                <td>Delete | Update </td>
+            </tr>
+        </tbody>
+    </table>
+    <div v-else class="alert alert-warning">No Locations Added yet</div>
+</template>
+
+<script>
+export default {
+    props: ['allLocations'],
+}
+</script>
+
+<style>
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,36 +2,51 @@
     <NavbarComponent></NavbarComponent>
     <div class="home">Home</div>
     <div class="text-end">
-            <p class="text-end">welcome {{ userName }}</p>
-    <router-link :to="{name: 'profile'}">
-        <button class="btn btn-info" type="button">Profile</button>
-    </router-link>
+        <p class="text-end">welcome {{ userName }}</p>
+        <router-link :to="{ name: 'profile' }">
+            <button class="btn btn-info" type="button">Profile</button>
+        </router-link>
     </div>
 
     <AddNewLocationComponent></AddNewLocationComponent>
-    
+    <UserLocationComponent
+        :allLocations="listOfLocations"
+    ></UserLocationComponent>
 </template>
 
 <script>
 import NavbarComponent from '../components/Header/NavbarComponent'
 import AddNewLocationComponent from '@/components/Location/AddNewLocationComponent'
+import UserLocationComponent from '@/components/Location/UserLocationComponent'
 import { mapActions } from 'vuex'
+import axiosInstance from '@/axiosInstance'
+
 export default {
     components: {
         NavbarComponent,
-        AddNewLocationComponent
+        AddNewLocationComponent,
+        UserLocationComponent,
     },
     data() {
         return {
             userName: '',
+            user_id: '',
+            listOfLocations: [],
         }
     },
-    mounted() {
+    async mounted() {
         let user = localStorage.getItem('user')
         if (!user) {
             this.redirectTo({ name: 'signup' })
         } else {
             this.userName = JSON.parse(user).name
+            this.user_id = JSON.parse(user).id
+        }
+        let results = await axiosInstance.get(
+            `/locations?user_id=${this.user_id}`
+        )
+        if (results.status == 200 && results.data.length > 0) {
+            this.listOfLocations = results.data
         }
     },
     methods: {
